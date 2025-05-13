5d7b5494d68f67bf3cfdc0b9a44178e26b249801
Xaver-DaRed <xaver.entropia@gmail.com>
2025-05-13 19:36:26 +0200

    Add bones for initial cutscene

---
+++ scripts/globals/moblin_maze_mongers/mmm_tutorial.lua
@@ -0,0 +1,25 @@
+-----------------------------------
+-- Moblin Maze Monglers: Tutorial (Goldagrik)
+-----------------------------------
+require('scripts/globals/npc_util')
+-----------------------------------
+xi = xi or {}
+xi.mmm = xi.mmm or {}
+
+xi.mmm.tutorialOnTrigger = function(player)
+    if player:hasKeyItem(xi.ki.TATTERED_MAZE_MONGER_POUCH) then
+        local ccPoints = xi.mmm.calculateCCPoints(player)
+
+        player:startEvent(20006, ccPoints)
+    else
+        player:startEvent(20004)
+    end
+end
+
+xi.mmm.tutorialOnEventFinish = function(player, csid, option)
+    if csid == 20004 and option == 1 then
+        if npcUtil.giveItem(player, { xi.item.MAZE_TABULA_M01, xi.item.MAZE_VOUCHER_01, xi.item.MAZE_RUNE_106, xi.item.MAZE_RUNE_109 }) then
+            npcUtil.giveKeyItem(player, xi.ki.TATTERED_MAZE_MONGER_POUCH)
+        end
+    end
+end
